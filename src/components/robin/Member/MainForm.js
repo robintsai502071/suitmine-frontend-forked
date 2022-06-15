d2f04cd3306d674b015f556b35b59719f1a9ba80
robin <robininispan@gmail.com>
2022-06-15 20:41:04 +0800

    [feat] 新增 Sidebar.js 未包含 RWD、MainForm.js 待續

--- src/components/robin/Member/MainForm.js
+++ src/components/robin/Member/MainForm.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
 
 function MainForm() {
   return (
-    <div>MainForm</div>
-  )
+    <div>
+      <div class="row">
+        <div class="col">
+          <label htmlFor="username">中文姓名</label>
+        </div>
+        <div class="col">
+          <input
+            type="text"
+            class="form-control"
+            id="username"
+            value={`蔡洧洧`}
+          />
+        </div>
+
+        <div class="col">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Last name"
+            aria-label="Last name"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default MainForm
+export default MainForm;
--- src/pages/Member.js
+++ src/pages/Member.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import MainForm from '../components/robin/Member/MainForm';
 import SideBar from '../components/robin/Member/SideBar';
 
 function Member() {
@@ -8,6 +9,9 @@ function Member() {
         <div className="row">
           <div className="col-2">
             <SideBar />
+          </div>
+          <div className="col-10">
+            <MainForm />
           </div>
         </div>
       </div>

e7779b9dde017890b7d86d82ee875dfaad7f1836
robin <robininispan@gmail.com>
2022-06-15 15:47:30 +0800

    [update] 新增 font-awesome cdn 至 index.html、新增英文字體 mixin 至 _global.scss

---
+++ src/components/robin/Member/SideBar.js
@@ -0,0 +1,16 @@
+function SideBar() {
+  return (
+    <>
+      <div className="main__sidebar">
+        <h5 className="main__sidebar__username">br502071</h5>
+        <button type="button" className="main__sidebar__edit-btn">
+          編輯檔案
+        </button>
+
+        
+      </div>
+    </>
+  );
+}
+
+export default SideBar;

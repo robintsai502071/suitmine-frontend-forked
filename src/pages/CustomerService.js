26396ce2772ade575c1d8446f34183454eb2d5bf
Alden <aldenwangf2e@gmail.com>
2022-06-16 22:48:27 +0800

    [Update]完成所有基本表單和樣式修改

--- src/pages/CustomerService.js
+++ src/pages/CustomerService.js
@@ -1,70 +1,71 @@
 import React from 'react';
-import Form from 'react-bootstrap/Form';
-import InputHere from '../components/alden/InputHere';
 import Btn from '../components/alden/Btn';
-
+import SelectAntd from '../components/alden/SelectAntd';
+import InputAntd from '../components/alden/InputAntd';
+import TextAreaAntd from '../components/alden/TextAreaAntd';
+import UploadAntd from '../components/alden/UploadAntd';
+import customerServiceBanner from '../images/alden/CustomerService/customerServiceBanner.png';
+import customerServiceBanner_sm from '../images/alden/CustomerService/customerServiceBanner_sm.png';
 function CustomerService() {
-  const labelName = ['您的Email地址', '主旨'];
   const InputText = ['請輸入你的Email地址', '請輸入主旨'];
+  const SelectArray = ['這裡', '先放', '假資料'];
+
   return (
-    <>
+    <div className="bg">
+      {/* <!-- 圖片 --> */}
+      <div className="container-fluid banner">
+        <div className="bannerweb w-100">
+          <img src={customerServiceBanner} className="w-100" alt="" />
+        </div>
+        <div className="bannermobile w-100 d-sm-none d-flex">
+          <img src={customerServiceBanner_sm} className="" alt="" />
+        </div>
+      </div>
       {/* <!-- container --> */}
       <div class="container">
         <div className="row">
           {/* <!-- main --> */}
-          <main class="col-7 m-auto ">
+          <main class="col-sm-8 m-auto">
             <form action="">
               {/* <!--選擇問題--> */}
-              <div>
-                <div>
-                  <label for="exampleFormControlInput1" class="form-label ">
-                    請在下方選擇你的問題
-                  </label>
-                </div>
-                <select
-                  class="form-select rounded-0"
-                  aria-label="Default select example rounded-0"
-                >
-                  <option selected>一般諮詢</option>
-                  <option value="1">One</option>
-                  <option value="2">Two</option>
-                  <option value="3">Three</option>
-                </select>
+              <div className="titles">
+                <h6>請在下方選擇你的問題</h6>
               </div>
+              <SelectAntd SelectArray={SelectArray} />
 
               {/* <!--Email--> */}
-              <InputHere labelName={labelName[0]} InputText={InputText[0]} />
+              <div className="titles">
+                <h6>您的Email地址</h6>
+              </div>
+              <InputAntd InputText={InputText[0]} />
 
               {/* <!--主旨--> */}
-              <InputHere labelName={labelName[1]} InputText={InputText[1]} />
+              <div className="titles">
+                <h6>主旨</h6>
+              </div>
+              <InputAntd InputText={InputText[1]} />
 
               {/* <!--請描述你的問題--> */}
-              <div>
-                <div>
-                  <label for="exampleFormControlTextarea1" class="form-label">
-                    請描述你的問題
-                  </label>
-                </div>
-                <textarea
-                  class="form-control rounded-0"
-                  id="exampleFormControlTextarea1"
-                  rows="9"
-                ></textarea>
+              <div className="titles">
+                <h6>請描述你的問題</h6>
               </div>
+              <TextAreaAntd TextAreaText="請詳述您的問題" />
 
               {/* <!-- 上傳附件 --> */}
-              <Form.Group controlId="formFileMultiple" className="mb-5">
-                <Form.Label>上傳附件(非必要)</Form.Label>
-                <Form.Control type="file" multiple className="mt-3" />
-              </Form.Group>
+              <div className="titles">
+                <h6>上傳照片(非必要)</h6>
+              </div>
+              <UploadAntd />
 
               {/* <!-- 送出button --> */}
-              <Btn />
+              <div>
+                <Btn BtnText="送出" />
+              </div>
             </form>
           </main>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 

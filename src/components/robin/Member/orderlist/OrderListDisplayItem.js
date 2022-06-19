fe16b1ffae078f08f026f6e89048cb5422cc1fae
Robin <robininispan@gmail.com>
2022-06-19 15:20:36 +0800

    [update] 1. OrderListDisplayItem.js 改用 <a> 標籤包、調整訂單資訊字體顏色 2. 新增取消訂單 Modal

--- src/components/robin/Member/orderlist/OrderListDisplayItem.js
+++ src/components/robin/Member/orderlist/OrderListDisplayItem.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { useState } from 'react';
+import { Modal, Radio, Space } from 'antd';
 
 function OrderListDisplayItem() {
+  // Modal 開啟狀態
+  const [CancelOrderModal, setCancelOrderModal] = useState(false);
+  // 開啟 Modal 後 radio 初始值
+  const [radioInitialValue, setRadioInitialValue] = useState(1);
+
+  const onChange = (e) => {
+    console.log('radio checked', e.target.value);
+    setRadioInitialValue(e.target.value);
+  };
+
   return (
     <>
-      <div className="order-list-display-item">
+      <a className="order-list-display-item d-block">
         <div className="order-list-display-item__wrapper container">
           <div className="order-list-display-item__header">
             <div className="order-list-display-item__header__number">
@@ -40,11 +52,36 @@ function OrderListDisplayItem() {
           </div>
 
           <div className="order-list-display-item__footer">
-            <button>取消訂單</button>
+            <button
+              onClick={() => {
+                setCancelOrderModal(true);
+              }}
+            >
+              取消訂單
+            </button>
             <button>再買一次</button>
           </div>
         </div>
-      </div>
+      </a>
+
+      <Modal
+        title="取消購買原因"
+        centered
+        visible={CancelOrderModal}
+        onOk={() => setCancelOrderModal(false)}
+        onCancel={() => setCancelOrderModal(false)}
+        okText="取消訂單"
+        cancelText="返回"
+      >
+        <Radio.Group onChange={onChange} value={radioInitialValue}>
+          <Space direction="vertical">
+            <Radio value={1}>修改訂單內容</Radio>
+            <Radio value={2}>改變心意</Radio>
+            <Radio value={3}>出貨時間太久</Radio>
+            
+          </Space>
+        </Radio.Group>
+      </Modal>
     </>
   );
 }

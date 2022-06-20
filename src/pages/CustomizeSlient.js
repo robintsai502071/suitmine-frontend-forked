e5d5aba8d1cce733f5e2511a5117642a6254fef0
robin <robininispan@gmail.com>
2022-06-21 03:09:55 +0800

    [update] 新增 intro 需再小修改

--- src/pages/CustomizeSlient.js
+++ src/pages/CustomizeSlient.js
@@ -37,13 +37,14 @@ function CustomizeSlient() {
   };
   // lightbox
   const [visible, setVisible] = useState(false);
+
   return (
     <>
       <LayoutHeader />
       {/* 桌機板 banner */}
       <div className="banner d-none d-lg-block">
         <div className="banner__wrapper mx-auto position-relative">
-          <figure className="banner__wrapper__model position-relative">
+          <figure className="banner__wrapper__model position-relative w-75 mx-auto">
             <img
               src={require('../images/robin/Custom/banner/main.png')}
               alt=""
@@ -52,46 +53,45 @@ function CustomizeSlient() {
             <h1 className="banner__wrapper__model__title text-white position-absolute">
               你的風格<span className="d-block">自己作主</span>
             </h1>
+            <div className="banner__wrapper__controller position-absolute">
+              <Slider {...settings} className="lapel">
+                <div>
+                  <h4 className="text-white">標準領</h4>
+                </div>
+                <div>
+                  <h4 className="text-white">修身領</h4>
+                </div>
+                <div>
+                  <h4 className="text-white">劍領</h4>
+                </div>
+              </Slider>
+              <Slider {...settings} className="buttons my-4">
+                <div>
+                  <h4 className="text-white">單鈕扣</h4>
+                </div>
+                <div>
+                  <h4 className="text-white">雙鈕扣</h4>
+                </div>
+                <div>
+                  <h4 className="text-white">雙排扣</h4>
+                </div>
+              </Slider>
+              <Slider {...settings} className="pockets">
+                <div>
+                  <h4 className="text-white">標準口袋</h4>
+                </div>
+                <div>
+                  <h4 className="text-white">斜蓋口袋</h4>
+                </div>
+                <div>
+                  <h4 className="text-white">無蓋斜口袋</h4>
+                </div>
+                <div>
+                  <h4 className="text-white">無蓋口袋</h4>
+                </div>
+              </Slider>
+            </div>
           </figure>
-
-          <div className="banner__wrapper__controller position-absolute">
-            <Slider {...settings} className="lapel">
-              <div>
-                <h4 className="text-white">標準領</h4>
-              </div>
-              <div>
-                <h4 className="text-white">修身領</h4>
-              </div>
-              <div>
-                <h4 className="text-white">劍領</h4>
-              </div>
-            </Slider>
-            <Slider {...settings} className="buttons my-4">
-              <div>
-                <h4 className="text-white">單鈕扣</h4>
-              </div>
-              <div>
-                <h4 className="text-white">雙鈕扣</h4>
-              </div>
-              <div>
-                <h4 className="text-white">雙排扣</h4>
-              </div>
-            </Slider>
-            <Slider {...settings} className="pockets">
-              <div>
-                <h4 className="text-white">標準口袋</h4>
-              </div>
-              <div>
-                <h4 className="text-white">斜蓋口袋</h4>
-              </div>
-              <div>
-                <h4 className="text-white">無蓋斜口袋</h4>
-              </div>
-              <div>
-                <h4 className="text-white">無蓋口袋</h4>
-              </div>
-            </Slider>
-          </div>
         </div>
       </div>
 
@@ -159,7 +159,6 @@ function CustomizeSlient() {
                 <div className="intro1__lightbox__thumb position-relative">
                   <img
                     className="w-100"
-                    // src="./images/intro1/lightbox_thumb.webp"
                     src={require('../images/robin/Custom//intro1/lightbox_thumb.webp')}
                     alt=""
                   />
@@ -295,6 +294,40 @@ function CustomizeSlient() {
           </h5>
         </div>
       </div>
+
+      {/*  intro2  */}
+      <div className="container my-5">
+        <div className="intro2">
+          <div className="row">
+            <div className="col-lg-6">
+              <div className="intro2__info px-4 px-lg-0 d-lg-flex flex-lg-column justify-content-lg-center h-100">
+                <h2 className="mb-3">您的選擇</h2>
+                <h6 className="mb-5">
+                  我們獨特的定制流程允許您為您的西裝選擇您想要的確切定制。從鈕扣和開衩到口袋和翻領，你可以說出你想要的樣子，這就是我們將如何建造它。
+                </h6>
+
+                <h2 className="mb-3">多項選擇</h2>
+                <h6>
+                  我們所有的定制選項都是免費的，並包含在您的西裝價格中。個性化您的購買，直至最後一個細節，以便您獲得您想要的確切服裝。
+                </h6>
+              </div>
+            </div>
+
+            <div className="col-lg-6 d-none d-lg-flex">
+              <div className="intro2__lightbox w-100">
+                <div className="intro2__lightbox__thumb position-relative">
+                  <img
+                    className="w-100"
+                    src={require('../images/robin/Custom//intro2/lightbox_thumb.webp')}
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      
       <LayoutFooter />
     </>
   );

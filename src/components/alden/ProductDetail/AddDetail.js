86444405e448f646978838212025280e005ae65a
Alden <aldenwangf2e@gmail.com>
2022-06-21 19:45:38 +0800

    [feat]未完成商品細節(網頁版)-彈窗完成

--- src/components/alden/ProductDetail/AddDetail.js
+++ src/components/alden/ProductDetail/AddDetail.js
@@ -1,37 +1,67 @@
 import { Button, Modal } from 'antd';
 import { useState } from 'react';
+import Accordion from './Accordion';
 
 const AddDetail = () => {
-  const [isModalVisible, setIsModalVisible] = useState(false);
-
-  const showModal = () => {
-    setIsModalVisible(true);
-  };
-
-  const handleOk = () => {
-    setIsModalVisible(false);
-  };
-
-  const handleCancel = () => {
-    setIsModalVisible(false);
-  };
-
+  const [visible, setVisible] = useState(false);
   return (
     <>
-      <Button type="primary" onClick={showModal}>
+      <Button type="primary" onClick={() => setVisible(true)}>
         客製化細節
       </Button>
       <Modal
-        title="Basic Modal"
-        visible={isModalVisible}
-        onOk={handleOk}
-        onCancel={handleCancel}
-        wrapClassName="custom-detail"
+        title="Modal 1000px width"
         centered
+        visible={visible}
+        onOk={() => setVisible(false)}
+        onCancel={() => setVisible(false)}
+        width={1000}
+        className={'popSelectDetail'}
+        footer={null}
       >
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-10 m-auto popDetail">
+              {/* 標題 */}
+              <div className="title ">
+                <h3 className="text-center">你的客製化細節</h3>
+              </div>
+              {/* 商品顯示 */}
+              <div className="showProduct">
+                {/* 商品照片 */}
+                <div className="pdtImg">
+                  <img
+                    src={require('../../../images/alden/ProductDetail/productDetailMainImg.png')}
+                    alt=""
+                  />
+                </div>
+                {/* 商品名，價格 */}
+                <div className="pdtName">
+                  <div>
+                    <h5>Harrogate Gray Suit</h5>
+                  </div>
+                  <div>
+                    <h5>NT $3500</h5>
+                  </div>
+                </div>
+              </div>
+              {/* 上方黑banner */}
+              <div className="popBanner"></div>
+              <form action="">
+                {/* 細節內容手風琴 */}
+                <div className="Accordion">
+                  <Accordion />
+                </div>
+                {/* 儲存按鈕 */}
+                <div>
+                  <button type="button" class="btn btn-primary widthbtn">
+                    儲存細節
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
       </Modal>
     </>
   );

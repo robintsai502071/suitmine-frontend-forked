12a4f016f4262e16e23d3c7fbf3ec56dd7cdfac8
Alden <aldenwangf2e@gmail.com>
2022-06-27 22:18:15 +0800

    [fix]排版:區塊、文字太大修正

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -253,9 +253,9 @@ function ProductDetail() {
             </div>
             {/* 評論內容 */}
             <div className="buttonSection">
-              <h3>
+              <h5>
                 問題的關鍵看似不明確，但想必在諸位心中已有了明確的從對於美麗東西的知覺中體驗到快樂，並且用盡一切方法使美麗的東西體現在行動中。這段話雖短，卻足以改變人類的歷史。在人生的歷程中，顧客評論的出現是必然的。{' '}
-              </h3>
+              </h5>
             </div>
           </div>
           <div className="commentBlock">
@@ -276,9 +276,9 @@ function ProductDetail() {
             </div>
             {/* 評論內容 */}
             <div className="buttonSection">
-              <h3>
+              <h5>
                 問題的關鍵看似不明確，但想必在諸位心中已有了明確的從對於美麗東西的知覺中體驗到快樂，並且用盡一切方法使美麗的東西體現在行動中。這段話雖短，卻足以改變人類的歷史。在人生的歷程中，顧客評論的出現是必然的。{' '}
-              </h3>
+              </h5>
             </div>
           </div>
           <div className="commentBlock">
@@ -299,9 +299,9 @@ function ProductDetail() {
             </div>
             {/* 評論內容 */}
             <div className="buttonSection">
-              <h3>
+              <h5>
                 問題的關鍵看似不明確，但想必在諸位心中已有了明確的從對於美麗東西的知覺中體驗到快樂，並且用盡一切方法使美麗的東西體現在行動中。這段話雖短，卻足以改變人類的歷史。在人生的歷程中，顧客評論的出現是必然的。{' '}
-              </h3>
+              </h5>
             </div>
           </div>
           <div className="commentBlock">
@@ -322,9 +322,9 @@ function ProductDetail() {
             </div>
             {/* 評論內容 */}
             <div className="buttonSection">
-              <h3>
+              <h5>
                 問題的關鍵看似不明確，但想必在諸位心中已有了明確的從對於美麗東西的知覺中體驗到快樂，並且用盡一切方法使美麗的東西體現在行動中。這段話雖短，卻足以改變人類的歷史。在人生的歷程中，顧客評論的出現是必然的。
-              </h3>
+              </h5>
             </div>
           </div>
         </div>

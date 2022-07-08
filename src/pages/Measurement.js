21ba0c4932bfb6e64be4feeda56ca908759d7cf6
Robert <rl.84@icloud.com>
2022-07-08 11:48:16 +0800

    [style] Measurement.js小修改

--- src/pages/Measurement.js
+++ src/pages/Measurement.js
@@ -5,11 +5,17 @@ import MeasurementProfile from '../components/sol/measurement/MeasurementProfile
 import PredictiveMeasurements from '../components/sol/measurement/PredictiveMeasurements';
 import EaseOfMind from '../components/sol/measurement/EaseOfMind';
 import Cellphone from '../components/sol/measurement/Cellphone';
+import LayoutHeader from '../components/robert/LayoutHeader';
+import LayoutMain from '../components/robert/LayoutMain';
+import LayoutFooter from '../components/robert/LayoutFooter';
 
 function Measurement() {
   return (
     <div>
+      <LayoutHeader/>
       <FitMatters />
+      <LayoutMain/>
+
       <GetNoticed />
       <MeasurementProfile />
       <PredictiveMeasurements />

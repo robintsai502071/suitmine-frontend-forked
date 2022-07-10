dc668c1830c502836df50a0b049ca8a7788e5737
Alden <aldenwangf2e@gmail.com>
2022-07-10 16:44:08 +0800

    [merge]menber-giftCards MessageBtn style

--- src/components/robin/Member/my-gift-card/MyGiftCardDisplayItem.js
+++ src/components/robin/Member/my-gift-card/MyGiftCardDisplayItem.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import { Button, Drawer } from 'antd';
+import React, { useState } from 'react';
 
 function MyGiftCardDisplayItem(props) {
   const { id, giver, create_time, amount, message, is_used } = props;
+  const [visible, setVisible] = useState(false);
+
+  const showDrawer = () => {
+    setVisible(true);
+  };
+
+  const onClose = () => {
+    setVisible(false);
+  };
 
   return (
     <>
-      <div className="my-gift-card-display-item p-4">
-        <div className=" d-flex justify-content-between">
+      <div className="my-gift-card-display-item p-5 site-drawer-render-in-current-wrapper">
+        <div className=" d-flex justify-content-between ">
           {/* 圖片 */}
           <div className="my-gift-card-display-item__image-wrapper">
             <img
@@ -15,12 +25,11 @@ function MyGiftCardDisplayItem(props) {
               alt=""
             />
           </div>
-
           {/* 右方文字 */}
           <div className="my-gift-card-display-item__wrapper ">
             <div className="my-gift-card-display-item__info d-flex flex-column">
               <div className="my-gift-card-display-item__info__title my-1">
-                贈送會員： {giver}
+                <div>贈送會員： {giver}</div>
               </div>
 
               <div className="my-gift-card-display-item__info__date my-1">
@@ -30,12 +39,9 @@ function MyGiftCardDisplayItem(props) {
               <div className="my-gift-card-display-item__info__amount my-1">
                 禮物卡金額 ${amount}
               </div>
-
-              <div className="my-gift-card-display-item__info__counts my-1 ">
-                留給我的訊息：{message}
-              </div>
             </div>
 
+            {/* 按鈕 */}
             <div className="my-gift-card-display-item__btns mt-3 d-flex">
               <button className="btnss">回贈</button>
               <button
@@ -53,6 +59,35 @@ function MyGiftCardDisplayItem(props) {
               >
                 已使用
               </button>
+            </div>
+            <Drawer
+              title="Basic Drawer"
+              placement="right"
+              closable={false}
+              onClose={onClose}
+              visible={visible}
+              getContainer={false}
+              style={{
+                position: 'absolute',
+              }}
+            >
+              <p>Some contents...</p>
+            </Drawer>
+            {/* 抽屜按鈕 */}
+            <div
+              style={{
+                marginTop: 16,
+              }}
+              className="btnForMessageBox"
+            >
+              <Button
+                type="primary"
+                onClick={showDrawer}
+                shape="circle"
+                className="btnForMessage"
+              >
+                this
+              </Button>
             </div>
           </div>
         </div>

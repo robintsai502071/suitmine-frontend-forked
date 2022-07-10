00986ac2801e96ada77deb52ed6c08ccdc236466
Alden <aldenwangf2e@gmail.com>
2022-07-10 23:50:41 +0800

    [feat]feature/memberMyfavorite 完成顯示商品最愛

--- src/components/robin/Member/my-favorites/MyFavoritesDisplay.js
+++ src/components/robin/Member/my-favorites/MyFavoritesDisplay.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import MyFavoritesDisplayItem from './MyFavoritesDisplayItem';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
+import { API_URL } from '../../../../utils/config';
 
 function MyFavoritesDisplay() {
+  //從後端撈資料
+  const [getMyfavorite, setGetMyfavorite] = useState([]);
+  useEffect(() => {
+    let getStocks = async () => {
+      let response = await axios.get(`${API_URL}/myFavorite/1`);
+      setGetMyfavorite(response.data);
+    };
+    getStocks();
+  }, []);
+  const newGetMyfavorite = getMyfavorite.giftCards;
+
   return (
     <>
       <div className="my-favorites position-relative">
@@ -15,21 +30,18 @@ function MyFavoritesDisplay() {
         </div>
 
         <div className="my-favorites__display row">
-          <div className="col-6 col-lg-3">
-            <MyFavoritesDisplayItem />
-          </div>
-          <div className="col-6 col-lg-3">
-            <MyFavoritesDisplayItem />
-          </div>
-          <div className="col-6 col-lg-3">
-            <MyFavoritesDisplayItem />
-          </div>
-          <div className="col-6 col-lg-3">
-            <MyFavoritesDisplayItem />
-          </div>
-          <div className="col-6 col-lg-3">
-            <MyFavoritesDisplayItem />
-          </div>
+          {newGetMyfavorite?.map((v, i) => {
+            const { name, price, product_photo } = v;
+            return (
+              <div className="col-6 col-lg-3">
+                <MyFavoritesDisplayItem
+                  name={name}
+                  price={price}
+                  product_photo={product_photo}
+                />
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
--- src/components/robin/Member/my-favorites/MyFavoritesDisplayItem.js
+++ src/components/robin/Member/my-favorites/MyFavoritesDisplayItem.js
@@ -1,20 +1,21 @@
 import React from 'react';
 
-function MyFavoritesDisplayItem() {
+function MyFavoritesDisplayItem(props) {
+  const { name, price, product_photo } = props;
+  const newProduct_photo = `http://localhost:3001/${product_photo}`;
+
   return (
     <a href="#/" className="d-block my-favorites-display-item text-center my-3">
       <div className="my-favorites-display-item__image-wrapper">
         <img
-          className="my-favorites-display-item__image"
-          src="https://fakeimg.pl/250x130/"
+          className="my-favorites-display-item__image classForFit"
+          src={newProduct_photo}
           alt=""
         />
       </div>
 
-      <h6 className="my-favorites-display-item__title my-2">
-        迪士尼文具旗艦店 | 圖繪抗 UV 摺疊傘
-      </h6>
-      <div className="my-favorites-display-item__price">$320</div>
+      <h6 className="my-favorites-display-item__title my-2">{name}</h6>
+      <div className="my-favorites-display-item__price">${price}</div>
       <i className="fa-solid fa-heart my-2 heart-icon"></i>
     </a>
   );

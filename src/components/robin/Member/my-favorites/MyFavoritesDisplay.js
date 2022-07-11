d03decb2397ddeb3ef976fe6006651522543b53d
Alden <aldenwangf2e@gmail.com>
2022-07-11 11:37:25 +0800

    [fix]feature/memberMyfavorite My Favorite刪除功能

--- src/components/robin/Member/my-favorites/MyFavoritesDisplay.js
+++ src/components/robin/Member/my-favorites/MyFavoritesDisplay.js
@@ -6,16 +6,43 @@ import axios from 'axios';
 import { API_URL } from '../../../../utils/config';
 
 function MyFavoritesDisplay() {
+  let [arr, setArr] = useState([]);
   //從後端撈資料
   const [getMyfavorite, setGetMyfavorite] = useState([]);
   useEffect(() => {
     let getStocks = async () => {
       let response = await axios.get(`${API_URL}/myFavorite/1`);
       setGetMyfavorite(response.data);
+      console.log(response.data.my_favorites);
+      setArr(response.data.my_favorites);
     };
     getStocks();
   }, []);
+
+  useEffect(() => {
+    let getStocks = async () => {
+      let response = await axios.get(`${API_URL}/myFavorite/1`);
+      setGetMyfavorite(response.data);
+      console.log(response.data.my_favorites);
+      setArr(response.data.my_favorites);
+    };
+    getStocks();
+  }, [arr]);
+
   const newGetMyfavorite = getMyfavorite.my_favorites;
+  //刪除
+  async function handleDelete(e) {
+    let response = await axios.delete(
+      `${API_URL}/myFavorite/1?product_id=${e}`
+    );
+    let newGetMyfavoriteDEL = [...newGetMyfavorite].filter((v) => {
+      // console.log('新陣列', v.product_id);
+      // console.log('舊陣列', e);
+      return v.product_id !== e;
+    });
+    // console.log('newGetMyfavoriteDEL', newGetMyfavoriteDEL);
+    setArr(newGetMyfavoriteDEL);
+  }
 
   return (
     <>
@@ -30,14 +57,16 @@ function MyFavoritesDisplay() {
         </div>
 
         <div className="my-favorites__display row">
-          {newGetMyfavorite?.map((v, i) => {
-            const { name, price, product_photo } = v;
+          {arr?.map((v, i) => {
+            const { name, price, product_photo, product_id } = v;
             return (
               <div className="col-6 col-lg-3">
                 <MyFavoritesDisplayItem
                   name={name}
                   price={price}
                   product_photo={product_photo}
+                  handleDelete={handleDelete}
+                  product_id={product_id}
                 />
               </div>
             );
--- src/components/robin/Member/my-favorites/MyFavoritesDisplayItem.js
+++ src/components/robin/Member/my-favorites/MyFavoritesDisplayItem.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { useState } from 'react';
 
 function MyFavoritesDisplayItem(props) {
-  const { name, price, product_photo } = props;
+  const { name, price, product_photo, product_id, handleDelete } = props;
   const newProduct_photo = `http://localhost:3001/${product_photo}`;
+  // console.log(product_id);
 
   return (
     <a href="#/" className="d-block my-favorites-display-item text-center my-3">
@@ -16,7 +18,13 @@ function MyFavoritesDisplayItem(props) {
 
       <h6 className="my-favorites-display-item__title my-2">{name}</h6>
       <div className="my-favorites-display-item__price">${price}</div>
-      <button className="loveBtn" onClick="handleDelete">
+      <button
+        className="loveBtn"
+        onClick={async () => {
+          handleDelete(product_id);
+        }}
+        data-runner="1"
+      >
         <i className="fa-solid fa-heart my-2"></i>
       </button>
     </a>
